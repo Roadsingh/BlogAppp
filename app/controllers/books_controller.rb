a9f7adba94f2d2f752073bccccf9d0582920b82b
Roadsingh <roadsingh@preciousinfosystem@gmail.com>
2023-02-02 19:38:42 +0530

    performing crud

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,48 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to :controller => 'books', :action => 'index'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+
+    redirect_to :action => 'index'
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:name, :author)
+  end
+
+end
